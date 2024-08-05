aa3b7344c92de65b18c5a78109321d329b99c609
arvin <rwiiinzakeri@gmail.com>
2024-08-05 18:50:54 +0330

    v-1

--- guessnumber/v-0/index.js
+++ guessnumber/v-0/index.js
@@ -1,31 +1,40 @@
+debugger
 // validate func
 function validateData(parameter) {
-  if (!Boolean(parameter.trim())) {
-    alert("plz input something");
-    throw new Error("plz input something");
-    return;
+  if (!Number(parameter)) {
+    if (parameter.lenght > 0) {
+      alert("plz put number not a string");
+      throw new Error("plz put number not a string");
+    } else {
+      alert("plz input something");
+      throw new Error("plz input something");
+    }
+    // alert("plz input something");
+    // throw new Error("plz input something");
+    // return;
   }
-  if (!Number(parameter.trim())) {
-    alert("plz put number not a string");
-    throw new Error("plz put number not a string");
-  }
+  // if (!Number(parameter)) {
+  //   alert("plz put number not a string");
+  //   throw new Error("plz put number not a string");
+  // }
 
-  return Number(parameter);
+  // return Number(parameter);
 }
-
 // client range
-const clientRangeMin = validateData(prompt("number range min"));
-const clientRangeMax = validateData(prompt("number range max"));
+const clientRangeMin = validateData(+prompt("number range min"));
+const clientRangeMax = validateData(+prompt("number range max"));
+
 // random number
-const randomNumber = Math.floor(
-  Math.random() * (clientRangeMin, clientRangeMax)
+const randomNumber = Math.round(
+  Math.random() * (clientRangeMax - clientRangeMin) + clientRangeMin
 );
+
 // client chance
 let clientChance = 5;
 // loop to guess number count down until chance become 0
 while (clientChance > 0) {
   // client guess
-  const clientGuess = validateData(window.prompt("your guess"));
+  const clientGuess = window.prompt("your guess");
 
   // check the answer
 
@@ -35,6 +44,7 @@ while (clientChance > 0) {
     );
     break;
   }
+  console.log(clientGuess);
   // check if answer was bigger
   if (clientGuess > randomNumber) {
     window.alert("your answer is bigger than number");
---
+++ guessnumber/v-1/index.js
@@ -0,0 +1,48 @@
+// validate prompt's... check is not empty (trim , length) and not a string
+function validate(parameter) {
+  if (!parameter || !parameter.trim()) {
+    window.alert("plz enter something");
+    throw new Error("plz enter something");
+  } else if (!Number(parameter)) {
+    window.alert("plz enter a number not a string");
+    throw new Error("plz enter a number not a string");
+  }
+  return Number(parameter);
+}
+// client range min && convert to number
+const clientRangeMin = validate(window.prompt("min range"));
+// client range max && convert to number
+const clientRangeMax = validate(window.prompt("max range"));
+// choose random number
+const RandomNumber = Math.round(
+  Math.random() * (clientRangeMax, clientRangeMin) + clientRangeMin
+);
+console.log(clientRangeMin, clientRangeMax, RandomNumber);
+// define client chance
+let clientChance = 5;
+// check guess until client chance done
+for (clientChance; clientChance > 0; ) {
+  const clientGuess = validate(window.prompt("you'r guess"));
+  if (clientGuess === RandomNumber) {
+    window.alert(
+      `your wiiiin , correct answer was ${RandomNumber} and you'r answer was ${clientGuess} with ${clientChance} chance`
+    );
+    console.log(
+      `your wiiiin , correct answer was ${RandomNumber} and you'r answer was ${clientGuess} with ${clientChance} chance`
+    );
+    break;
+  }
+  if (clientGuess > RandomNumber) {
+    window.alert("you'r guess is bigger than computer number");
+    clientChance--;
+  } else {
+    window.alert("you'r guess is smaller than computer number");
+    clientChance--;
+  }
+}
+
+// check all prompt
+// const clientData = prompt("");
+// console.log(validate(clientData));
+
+// || parameter.length > 0 || parameter.trim()
